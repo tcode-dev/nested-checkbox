8afe7b5b1bb5fc3709b6769f19d7341b3c44d9b6
tkw.tokyo <tkw.tokyo@gmail.com>
2020-07-05 14:42:39 +0900

    リファクタ list

--- web/server.js
+++ web/server.js
@@ -2,100 +2,16 @@ const express = require("express");
 const ejs = require("ejs");
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-const layer1 = {
-    "group": "j-nestedCheckbox__group--layer1",
-    "trigger": "j-nestedCheckbox__trigger--layer1",
-    "name": "layer1_cd",
-};
-const layer2 = {
-    "group": "j-nestedCheckbox__group--layer2",
-    "trigger": "j-nestedCheckbox__trigger--layer2",
-    "name": "layer2_cd",
-};
-const layer3 = {
-    "group": "j-nestedCheckbox__group--layer3",
-    "trigger": "j-nestedCheckbox__trigger--layer3",
-    "name": "layer3_cd",
-};
-const layer4 = {
-    "group": "j-nestedCheckbox__group--layer4",
-    "trigger": "j-nestedCheckbox__trigger--layer4",
-    "name": "layer4_cd",
-};
-const list = [
-    {
-        "layer": layer1,
-        "value": "1",
-        "children": [
-            {
-                "layer": layer2,
-                "value": "1-1",
-                "children": []
-            },
-            {
-                "layer": layer2,
-                "value": "1-2",
-                "children": [
-                    {
-                        "layer": layer3,
-                        "value": "1-2-1",
-                        "children": []
-                    },
-                    {
-                        "layer": layer3,
-                        "value": "1-2-2",
-                        "children": []
-                    }
-                ]
-            },
-            {
-                "layer": layer2,
-                "value": "1-3",
-                "children": [
-                    {
-                        "layer": layer3,
-                        "value": "1-3-1",
-                        "children": []
-                    },
-                    {
-                        "layer": layer3,
-                        "value": "1-3-2",
-                        "children": [
-                            {
-                                "layer": layer4,
-                                "value": "1-3-2-1",
-                                "children": []
-                            },
-                            {
-                                "layer": layer4,
-                                "value": "1-3-2-2",
-                                "children": []
-                            },
-                            {
-                                "layer": layer4,
-                                "value": "1-3-2-3",
-                                "children": []
-                            }
-                        ]
-                    },
-                    {
-                        "layer": layer3,
-                        "value": "1-3-3",
-                        "children": []
-                    }
-                ]
-            }
-        ]
-    }
-];
+const List = require('./app/mock/List');
 
 app.set('views', __dirname + '/app/view');
 
 app.engine('ejs', ejs.renderFile);
 
 app.get("/", (req, res) => {
-    res.render('index.ejs', { list });
+    const list = new List();
+
+    res.render('index.ejs', { list: list.get() });
 });
 
 app.get("/api", (req, res) => {
