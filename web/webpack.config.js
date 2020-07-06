31797b8baf9f9198a57be412d792fb7c7439a747
tkw.tokyo <tkw.tokyo@gmail.com>
2020-07-07 08:26:25 +0900

    babel 追加

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -12,6 +12,24 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    useBuiltIns: 'usage',
+                                    corejs: 3,
+                                },
+                            ],
+                        ],
+                    },
+                },
+            },
+            {
                 test: /\.s[ac]ss$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
