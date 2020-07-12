3ca5054237b03c0dde44122612695580285b0a7b
tkw.tokyo <tkw.tokyo@gmail.com>
2020-07-13 08:11:26 +0900

    テストコードの構造を合わせる

--- web/app/js/module/__test__/UrlCreator.test.js
+++ web/app/js/module/__test__/UrlCreator.test.js
@@ -2,17 +2,18 @@ import assert from 'assert';
 import UrlCreator from '../UrlCreator';
 
 const url = 'http://localhost';
+describe('UrlCreator', () => {
+    describe('create', () => {
+        test('パラメータが?で連結されること', () => {
+            const urlCreator = new UrlCreator(url, { a: 1 });
 
-describe('create', () => {
-    test('パラメータが?で連結されること', () => {
-        const urlCreator = new UrlCreator(url, { a: 1 });
+            assert.strictEqual(urlCreator.create(), `${url}?a=1`);
+        });
 
-        assert.strictEqual(urlCreator.create(), `${url}?a=1`);
-    });
+        test('パラメータが&で連結されること', () => {
+            const urlCreator = new UrlCreator(`${url}?a=1`, { b: 1 });
 
-    test('パラメータが&で連結されること', () => {
-        const urlCreator = new UrlCreator(`${url}?a=1`, { b: 1 });
-
-        assert.strictEqual(urlCreator.create(), `${url}?a=1&b=1`);
+            assert.strictEqual(urlCreator.create(), `${url}?a=1&b=1`);
+        });
     });
 });
