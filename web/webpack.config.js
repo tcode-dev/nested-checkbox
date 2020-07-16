880cb88c0109b67093b42d504854627effcf5141
tkw.tokyo <tkw.tokyo@gmail.com>
2020-07-17 08:42:13 +0900

    webpack dev、prodのコマンド追加

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,71 +3,73 @@ const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-module.exports = {
-    entry: {
-        'js/TopController': './app/js/top/TopController.js',
-        'css/top/index': './app/scss/top/index.scss',
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    useBuiltIns: 'usage',
-                                    corejs: 3,
-                                },
+module.exports = (env) => {
+    return {
+        entry: {
+            'js/TopController': './app/js/top/TopController.js',
+            'css/top/index': './app/scss/top/index.scss',
+        },
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: '[name].js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        useBuiltIns: 'usage',
+                                        corejs: 3,
+                                    },
+                                ],
                             ],
-                        ],
+                        },
                     },
                 },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                          sourceMap: true,
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
                         },
-                      },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            plugins: [
-                                Autoprefixer({
-                                    grid: true,
-                                }),
-                            ],
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                                plugins: [
+                                    Autoprefixer({
+                                        grid: true,
+                                    }),
+                                ],
+                            },
                         },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
                         },
-                    },
-                ],
-            },
+                    ],
+                },
+            ],
+        },
+        devtool: env.prod ? false : 'source-map',
+        plugins: [
+            new FixStyleOnlyEntriesPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+            }),
         ],
-    },
-    devtool: 'source-map',
-    plugins: [
-        new FixStyleOnlyEntriesPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-    ],
+    };
 };
